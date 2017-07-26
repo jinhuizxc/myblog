07f22e7b65840eca3ac6acb5f48e57c295f68127
JinHui <hui.jin@boatchina.net>
2017-07-26 09:50:46 +0800

    博客主页面开发

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,10 @@ def index(request):
     # 1、加载响应
     # return HttpResponse('Hello world!')
     # 2、加载Html文件
-     article = models.Article.objects.get(pk=1)
-     return render(request, 'blog/index.html', {'article': article})
+    # 获取文章列表
+    #  article = models.Article.objects.get(pk=1)
+    # 获取所有的文章对象
+     articles = models.Article.objects.all()
+     return render(request, 'blog/index.html', {'articles': articles})
+
 
