be1f03703a3c5fa0e2e8ccec72dfa8a569e51575
JinHui <hui.jin@boatchina.net>
2017-07-26 16:31:25 +0800

    博客撰写页面开发（一），到这里出现错误，放一放吧

--- blog/views.py
+++ blog/views.py
@@ -10,10 +10,21 @@ def index(request):
     # 3、获取文章列表
     #  article = models.Article.objects.get(pk=1)
     # 4、获取所有的文章对象
+    # 博客主页面开发
      articles = models.Article.objects.all()
      return render(request, 'blog/index.html', {'articles': articles})
  # 博客文章页面开发
 def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
+# 博客撰写页面开发（一）
+def edit_page(request):
+    return render(request, 'blog/edit_page.html')
+# 编辑响应函数
+def edit_action(request):
+    title = request.Post.get('title', 'TITLE')
+    content = request.Post.get('content', 'CONTENT')
+    models.Article.objects.create(title = title, content = content)
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
 
